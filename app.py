ac505449aa65d074a5a42fbc7d0610959c4bcb86
Namo Kitchaivit <namo@Namos-MacBook-Pro.local>
2026-09-14 21:59:56 +0700

    Create initial Streamlit app

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Global Innovation Build Challenge", page_icon="🌍")
+
+st.title("Global Innovation Build Challenge")
+st.write("My app is running!")
